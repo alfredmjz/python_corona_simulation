b4840ee36598445e291c6f33c3c09e4179a3d0cb
Alfred Mah <alfredmjz@gmail.com>
2021-08-11 23:09:24 -0400

    change concrete implementation

--- bridgeDP.py
+++ bridgeDP.py
@@ -13,11 +13,12 @@ class SimulationAbstraction:
         
         self._environment = environment #the environment to run in
 
-    def updatePopulationSize(self, val):
-        self._environment.setPopulationSize(self._config, val)
-        
+
     def updateSimulationSteps(self, val):
         self._environment.setSimulationSteps(self._config, val)
+    
+    def toggleColorblind(self, mode, colorblind_type):
+        self._environment.setColorblind(self._config, mode, colorblind_type)
         
     def instantiate(self): 
         self._environment.produce_population(self._simulation)
@@ -31,15 +32,20 @@ class SimulationVariants(SimulationAbstraction):
     Extends the SimulationAbstraction without changing the Implementation classes.
     Provides additional variants besides the base in SimulationAbstraction
     """
+    
+    def updatePopulationSize(self, val):
+        self._environment.setPopulationSize(self._config, val)
+        
+    def updatePopulationAge(self, mean_age, max_age):
+        self._environment.setPopulationAge(self._config, mean_age, max_age)
+    
+    def enable_ageRiskDependencies(self, risk_age, critical_age, critical_mortality_chance, risk_increase):
+        self._environment.ageRiskDependencies(self._config, risk_age, critical_age, 
+                                              critical_mortality_chance, risk_increase)
+        
     def noHealthcare(self):
         self._environment.setHealthcareCapacity(self._config, 0)
     
-    def increaseInfectionChance(self, percent):
-        self._environment.setInfectionChance(self._config, percent)
-    
-    def toggleColorblind(self, mode, colorblind_type):
-        self._environment.setColorblind(self._config, mode, colorblind_type)
-        
     def applyLockdown(self, lockdown_percentage, lockdown_compliance):
         self._environment.setLockdownVariables(self._config, lockdown_percentage, lockdown_compliance)
         self._config.set_lockdown(lockdown_percentage=0.1, lockdown_compliance=0.9)
@@ -54,13 +60,13 @@ class SimulationVariants(SimulationAbstraction):
         self._environment.setReducedInteraction(self._config, speed)
         self._config.set_reduced_interaction(speed = 0.001) 
         
+
 class Implementator(ABC):
     """
     The Implementator defines the interface for all implementation classes. It
     doesn't have to match the Abstraction's interface.
     """
-    @abstractmethod
-    def setPopulationSize(): pass
+
     
     @abstractmethod
     def setSimulationSteps() : pass
@@ -80,40 +86,32 @@ Each Concrete Implementation corresponds implements
 the Implementation interface
 """
 
-class highDensityPopulation(Implementator):
+class populationDependent(Implementator):
     """
     The Abstraction maintains a reference to an object of the
     Implementation hierarchy and delegates all of the real work to this object.
     """
 
-    def setPopulationSize(config, val=6000):
+    def setPopulationSize(config, val):
         config.pop_size = val
     
+    def setPopulationAge(config, mean_age, max_age):
+        config.mean_age = mean_age
+        config.max_age = max_age
+    
+    def ageRiskDependencies(config, risk_age, critical_age, critical_mortality_chance, risk_increase):
+        config.age_dependent_risk = True
+        config.risk_age = risk_age
+        config.critical_age = critical_age
+        config.critical_mortality_chance = critical_mortality_chance
+        config.risk_increase = risk_increase
+        
     def setSimulationSteps(config, val):
         config.simulation_steps = val
-    
     
     def setColorblind(config, mode, colorblind_type="deuteranopia"):
         config.colorblind_mode = mode
         config.colorblind_type = colorblind_type
-        
-    def setHealthcareCapacity(config, val):
-        config.healthcare_capacity = val
-    
-    def setInfectionChance(config, percent):
-        config.infection_chance *= percent
-        
-    def setLockdownVariables(config, lockdown_percentage, lockdown_compliance):
-        config.lockdown_percentage = lockdown_percentage
-        config.lockdown_compliance = lockdown_compliance
-        
-    def setSelfIsolationVariables(config, self_isolate_proportion, isolation_bounds, traveling_infects): 
-        config.self_isolate_proportion = self_isolate_proportion
-        config.isolation_bounds = isolation_bounds
-        config.traveling_infects = traveling_infects
-        
-    def setReducedInteractionVariables(config, speed): 
-        config.speed = speed
         
     def produce_population(simulation):
         simulation.population_init()
@@ -123,19 +121,7 @@ class highDensityPopulation(Implementator):
         
     
 
-class lowDensityPopulation(Implementator):
-    def setPopulationSize(config, val=500):
-        config.pop_size = val
-    
-    def setSimulationSteps(config, val):
-        config.simulation_steps = val
-    
-    def setHealthcareCapacity(config, val):
-        config.healthcare_capacity = val
-    
-    def setInfectionChance(config, percent):
-        config.infection_chance *= percent
-        
+class scenarioDependent(Implementator):
     def setLockdownVariables(config, lockdown_percentage, lockdown_compliance):
         config.lockdown_percentage = lockdown_percentage
         config.lockdown_compliance = lockdown_compliance
@@ -147,6 +133,16 @@ class lowDensityPopulation(Implementator):
         
     def setReducedInteractionVariables(config, speed): 
         config.speed = speed
+
+    def setHealthcareCapacity(config, val):
+        config.healthcare_capacity = val
+       
+    def setSimulationSteps(config, val):
+        config.simulation_steps = val
+    
+    def setColorblind(config, mode, colorblind_type="deuteranopia"):
+        config.colorblind_mode = mode
+        config.colorblind_type = colorblind_type
         
     def produce_population(simulation):
         simulation.population_init()
@@ -162,18 +158,19 @@ if __name__ == "__main__":
     implementation combination.
     """
 
-    environment = highDensityPopulation
-    SIMULATION1 = SimulationAbstraction(environment)
+    environment = populationDependent
+    SIMULATION1 = SimulationVariants(environment)
     SIMULATION1.updateSimulationSteps(1000)
     SIMULATION1.updatePopulationSize(5000)
+    SIMULATION1.updatePopulationAge(20,80)
+    SIMULATION1.enable_ageRiskDependencies(35, 50, 0.3, "linear")
     SIMULATION1.instantiate()
 
     
-    environment = lowDensityPopulation
+    environment = scenarioDependent
     SIMULATION2 = SimulationVariants(environment)
-    SIMULATION2.increaseInfectionChance(0.2) 
     SIMULATION2.updateSimulationSteps(1000)
-    SIMULATION2.applyLockdown(0.5,0.9)
+    SIMULATION2.applyLockdown(0.5, 0.9)
     SIMULATION2.noHealthcare()
     SIMULATION2.instantiate()
 
